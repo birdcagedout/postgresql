314d0c7afaa8e626540d1534a6b37f3f49c00451
birdcagedout <birdcagedout@gmail.com>
2024-08-08 19:05:46 +0900

    20240808_work2

--- postgresql.sql
+++ postgresql.sql
@@ -23,3 +23,20 @@
 delete from topic where id = 4;
 
 select * from topic;
+
+
+ALTER TABLE 테이블이름
+ADD CONSTRAINT 제약조건이름 제약종류 (적용할 열);
+
+ALTER TABLE 테이블이름
+ADD 제약종류 (적용할 열);
+
+
+ALTER TABLE 테이블이름
+DROP CONSTRAINT 제약조건이름;
+
+ALTER TABLE 테이블이름
+ALTER COLUMN 열이름 SET NOT NULL;
+
+ALTER TABLE 테이블이름
+ALTER COLUMN 열이름 DROP NOT NULL;
